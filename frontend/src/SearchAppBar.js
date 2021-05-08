5345f21fefe1112c3ddae33c7736c73f429f28d2
Michael Leung <michaelhtleung@gmail.com>
2021-05-07 22:02:41 -0400

    Add static text search bar

--- frontend/src/SearchAppBar.js
+++ frontend/src/SearchAppBar.js
@@ -70,17 +70,6 @@ export default function SearchAppBar() {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        Material-UI
-                    </Typography>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon />
