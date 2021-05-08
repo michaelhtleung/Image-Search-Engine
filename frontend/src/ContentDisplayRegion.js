4125d60e5318283e0cddde6758aeffa6bb6ab189
Michael Leung <michaelhtleung@gmail.com>
2021-05-08 06:15:26 -0400

    Add no results message

--- frontend/src/ContentDisplayRegion.js
+++ frontend/src/ContentDisplayRegion.js
@@ -1,18 +1,32 @@
 import ImageCard from "./ImageCard";
+import Typography from '@material-ui/core/Typography';
 
 export default function ContentDisplayRegion(props) {
-    return (
-        <div>
-            <ul>
-                {props.imageCardData.data.map(cardData => {
-                    return (
-                        <ImageCard
-                            cardData={cardData}
-                            key={cardData.image_id.toString()}>
-                        </ImageCard>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+    if (props.imageCardData.data.length == 0) {
+        return (
+            <div>
+                <Typography variant="h4">
+                    No matching images found.
+                </Typography>
+                <Typography variant="h6">
+                    Please try another search query.
+                </Typography>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <ul>
+                    {props.imageCardData.data.map(cardData => {
+                        return (
+                            <ImageCard
+                                cardData={cardData}
+                                key={cardData.image_id.toString()}>
+                            </ImageCard>
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
 }
