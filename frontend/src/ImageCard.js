961ca655a647b3b2f4394fcb8c8303b1a65a5a57
Michael Leung <michaelhtleung@gmail.com>
2021-05-09 14:55:00 -0400

    Trim ImgaeCard imports

--- frontend/src/ImageCard.js
+++ frontend/src/ImageCard.js
@@ -1,25 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import profilePicture1 from "./static/alice.jpg";
 import profilePicture2 from "./static/bob.jpg";
-import contentPicture1 from "./static/blue_chair.jpg";
-import contentPicture2 from "./static/brown_coat.jpg";
-import contentPicture3 from "./static/red_purse.jpg";
 import moment from 'moment';
 
 const useStyles = makeStyles((theme) => ({
